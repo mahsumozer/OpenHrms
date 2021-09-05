5f9833000646dd42c99e4f44891a5ff48bec5b48
mahsumozer <46961602+mahsumozer@users.noreply.github.com>
2021-09-05 23:04:31 +0300

    Update Constants.java

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -1,9 +1,9 @@
 package utils;
 
 public class Constants {
-    public static final String CONFIGURATION_FILEPATH ="C:\\Users\\omahs\\Desktop\\configuration.properties";
-    public static final String TESTDATA_FILEPATH = "C:\\Users\\omahs\\Desktop\\SampleTestData.xlsx";
-    public static final String CHROMEDRIVER_PATH="C:\\Users\\omahs\\Desktop\\chromedriver.exe";
+    public static final String CONFIGURATION_FILEPATH ="C:\\Users\\**Username**\\**Current place**\\configuration.properties";
+    public static final String TESTDATA_FILEPATH = "C:\\Users\\**Username**\\**Current place**\\SampleTestData.xlsx";
+    public static final String CHROMEDRIVER_PATH="C:\\Users\\**Username**\\**Current place**\\chromedriver.exe";
     public static final String GECKODRIVER_PATH=System.getProperty("user.dir")+"/src/test/resources/drivers/chromedriver";
     public static final int IMPLICIT_WAIT_TIME=20;
     public static final int EXPLICIT_WAIT_TIME=20;
